f16237314bffd10fd5484705b697f68aa74659e3
ndubuisiifeanyi828 <ndubuisiifeanyi828@gmail.com>
2025-10-19 15:06:08 -0700

    Made navbar responsive and fixed layout

--- src/component/CardNav.jsx
+++ src/component/CardNav.jsx
@@ -1,19 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CardNav.css";
 
-
 const CardNav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className="navbar">
-      <div className="nav-logo">🍴 BiteCraft Kitchen</div>
-      <ul className="nav-links">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Menu</a></li>
-        <li><a href="#">Gallery</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Contact</a></li>
-      </ul>
-      <button className="nav-btn">Order Now</button>
+      <div className="logo">BiteCraft Kitchen</div>
+
+      <div className={`nav-links ${isOpen ? "open" : ""}`}>
+        <a href="#">Home</a>
+        <a href="#">Menu</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+      </div>
+
+      <div
+        className="hamburger"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
     </nav>
   );
 };
